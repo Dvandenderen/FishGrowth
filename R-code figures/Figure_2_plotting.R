4dd3df9467c542caf101a75066f6c19cd5db224e
Pieter Daniël van Denderen <pdvd@win.dtu.dk>
2020-01-20 16:57:29 +0100

    update

---
+++ R-code figures/Figure_2_plotting.R
@@ -0,0 +1,54 @@
+# illustration of growth coefficient A with von Bertalanffy curves
+###################################################################
+  A <- c(5,5,10,10) # growth coefficient A
+  f <- c(0.5,2,0.5,2) # loss coefficient f
+  n <- 2/3 # setting the exponent
+  idx1 <- c(1,3,5,7) # setting index1 for loop
+  idx2 <- c(2,4,6,8) # setting index2 for loop
+  #(A/f)^3 # maximum weight
+
+# set an empty data matrix
+  w <- matrix(NA,nrow=21,ncol=8) # matrix with empty data
+  w[1,c(1,3,5,7)] <- 0.01        # weight at age
+  w[1,c(2,4,6,8)] <- NA          # specific weight at age
+
+# calculate the weight and specific weight over time
+  for (j in 1:4){
+    for (i in 1:20){
+    change <- A[j] * w[i,idx1[j]]^n - f[j] * w[i,idx1[j]]
+    w[i+1,idx1[j]] <- change + w[i,idx1[j]]
+    w[i,idx2[j]] <-  change
+    }}
+  
+  setwd("C:/Users/pdvd/Online for git/FishGrowth/Output")
+  pdf("Growth_illustration.pdf",width=6.9,height=3.56)
+  par(mfrow=c(1,2), mar=c(4, 4, 2, 1))
+  
+  l1 <- (w[,1]/0.01)^(1/3)
+  plot(l1,type="l",lwd=2,ylim=c(1,100),ylab="length (cm)",xlab= "Time (years)", yaxt="n", xaxt="n",col="grey")
+  l3 <- (w[,3]/0.01)^(1/3)
+  lines(l3,col="#ef3b2c",lwd=2)
+  l5 <- (w[,5]/0.01)^(1/3)
+  lines(l5,col="grey",lwd=2,lty=2)
+  l7 <- (w[,7]/0.01)^(1/3)
+  lines(l7,col="#ef3b2c",lwd=2,lty=2)
+  axis(1, c(1,11,21),c("0","10","20"))
+  axis(2,c(0,50,100),las=1)
+  
+  library(latex2exp)
+  plot((w[,2]/w[,1]), xlim=c(1,9), ylim=c(0,50), type ="l", xlab="Time (years)",
+       ylab=TeX("Specific growth (w$_{t+1}$-w$_t$)/w$_t$)"),xaxt="n",yaxt="n",lwd=2,col="grey")
+  
+  axis(1,c(1,5,9),c("0","4","8"))
+  axis(2,c(0,25,50),las=1)
+  lines((w[,4]/w[,3]), col="#ef3b2c",lwd=2)
+  lines((w[,6]/w[,5]), col="grey",lty=2,lwd=2)
+  lines((w[,8]/w[,7]), col="#ef3b2c",lty=2,lwd=2)
+  
+  legend(4.5,50, legend=c("A = 5, f = 0.5", "A = 5, f = 2", "A = 10, f = 0.5","A = 10, f = 2"),
+         col=c("grey","#ef3b2c","grey","#ef3b2c"), lty=c(1,1,2,2),lwd=c(2,2,2,2), cex=0.8,
+         box.lty=0, y.intersp=1)
+  
+  dev.off()
+
+
--- R-code figures/Figure_4_plotting.R
+++ R-code figures/Figure_4_plotting.R
@@ -1,127 +1,135 @@
-
-#### plot fish growth and temperature - Figure 3
+#### plot fish growth and temperature - Figure 4
 ####################################################
-  setwd("C:/Users/pdvd/Online for git/FishGrowth/R-code processing")
-  load("Processed_files.Rdata")
-  M3 <- sumdata[[3]]
-  fishes <- sumdata[[7]]
+  setwd("C:/Users/pdvd/Online for git/FishGrowth/R-code statistics")  
+  source("Main_analyses.R")
   
-  setwd("C:/Users/pdvd/Online for git/FishGrowth/Output") # path for figures
   library(latex2exp)  
+  library(merTools)
 
-  pdf("Illustration_growth.pdf",width=4,height=5.2)  
-  
-# get the prediction from M3
-  meanfish  <- colMeans(M3)
-  
-# get Q10 output in matrix
-  Q10data <- matrix(data = NA, ncol =2, nrow =4)
-  colnames(Q10data) <- c("100cm","30cm")
-  rownames(Q10data) <- c("pel","deep","dem","shray")
-    
-# calculate temp effect + Q10 for asymptoic length 100 cm fish
-  Te <- seq(0,30)
-  Li <- rep(log10(100),length(Te))
-  tz <- rep(0,length(Te))
+# get 95% CI for coefficients for M1 model
+  M1_prof <- profile(M1)
+  M1_unc <- confint(M1_prof,level=0.95)
 
-  ypel  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-           meanfish["groupingPEL"]+meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li + 
-           meanfish["Temperature:groupingPEL"]*Te
-  Q10data[1,1] <- (10^(ypel[31])/10^(ypel[1]))^(10/(30-0))
+  setwd("H:/Werk/BP food web model/191213 mixed model centering")
+# part A - all fishes
+  pdf("Growth_allfish.pdf",width=6.9,height=3.56)  
+  par(mfrow=c(1,2), mar=c(4, 4, 2, 1))
+  plot(LArate~Temperature, data=fishes,ylim=c(-0.4,2.5),las=1,xlim=c(-2,30),cex=1,
+       yaxt="n",xaxt="n", xlab=TeX("Temperature ($^o$C)"),
+       ylab="Growth coef. A")
+
+# get the across species prediction from M1
+  T_across <- seq(min(fishes$Temperature),max(fishes$Temperature),0.1)
+  Name <- rep(NA,length(T_across))
+  uniReg <- rep(NA, length(T_across))
+  T_within <- rep(0,length(T_across))
+  newdat <- data.frame(T_across,Name,T_within,uniReg)
+  yn <- predict(M1, newdata = newdat, re.form = NA)
   
-  ydeep  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-            meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li
-  Q10data[2,1] <- (10^(ydeep[31])/10^(ydeep[1]))^(10/(30-0))
+  lines(yn~T_across,col="blue",lwd=2)
+
+# calculate the 95% confidence interval
+  pred <- bootMer(M1, function(x) predict(x, newdata = newdat, re.form = NA), nsim = 100)
+  CI.lower = apply(pred$t, 2, function(x) as.numeric(quantile(x, probs=.025, na.rm=TRUE)))
+  CI.upper = apply(pred$t, 2, function(x) as.numeric(quantile(x, probs=.975, na.rm=TRUE)))
+  lines(CI.upper~T_across,lty=2,col="red")
+  lines(CI.lower~T_across,lty=2,col="red")
   
-  ydem  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-           meanfish["groupingDEM"]+meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li + 
-           meanfish["Temperature:groupingDEM"]*Te
-  Q10data[3,1] <- (10^(ydem[31])/10^(ydem[1]))^(10/(30-0))
+# get Q10 between species 
+  T_across <- c(10,20)
+  Name <- rep(NA,length(T_across))
+  uniReg <- rep(NA, length(T_across))
+  T_within <- rep(0,length(T_across))
+  newdat <- data.frame(T_across,Name,T_within,uniReg)
+  yq10 <- predict(M1, newdata = newdat, re.form = NA)
+  10^(yq10[2])/10^(yq10[1])
+  yq10_lower <- M1_unc[4,1]+M1_unc[6,1]*T_across
+  10^(yq10_lower[2])/10^(yq10_lower[1])
+  yq10_upper <- M1_unc[4,2]+M1_unc[6,2]*T_across
+  10^(yq10_upper[2])/10^(yq10_upper[1])
   
-  yshray  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-             meanfish["groupingSHRAY"]+meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li + 
-             meanfish["Temperature:groupingSHRAY"]*Te
-  Q10data[4,1] <- (10^(yshray[31])/10^(yshray[1]))^(10/(30-0))
+# get Q10 within species
+  T_within <- c(10,20)
+  Name <- rep(NA,length(T_within))
+  uniReg <- rep(NA, length(T_within))
+  T_across <- rep(0,length(T_within))
+  newdat <- data.frame(T_within,Name,T_within,uniReg)
+  yq10 <- predict(M1, newdata = newdat, re.form = NA)
+  10^(yq10[2])/10^(yq10[1])
+  yq10_lower <- M1_unc[4,1]+M1_unc[5,1]*T_within
+  10^(yq10_lower[2])/10^(yq10_lower[1])
+  yq10_upper <- M1_unc[4,2]+M1_unc[5,2]*T_within
+  10^(yq10_upper[2])/10^(yq10_upper[1])
   
-# calculate temp effect + Q10 for asymptoic length 30 cm fish 
-  Te <- seq(0,30)
-  Li <- rep(log10(30),length(Te))
-  tz <- rep(0,length(Te))
-  
-  yspel  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-            meanfish["groupingPEL"]+meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li + 
-            meanfish["Temperature:groupingPEL"]*Te
-  Q10data[1,2] <- (10^(yspel[31])/10^(yspel[1]))^(10/(30-0))
-  
-  ysdeep  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-             meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li
-  Q10data[2,2] <- (10^(ysdeep[31])/10^(ysdeep[1]))^(10/(30-0))
-  
-  ysdem  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-    meanfish["groupingDEM"]+meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li + 
-    meanfish["Temperature:groupingDEM"]*Te
-  Q10data[3,2] <- (10^(ysdem[31])/10^(ysdem[1]))^(10/(30-0))
-  
-  ysshray  <- meanfish["Intercept"]+meanfish["Temperature"]*Te+meanfish["LLinf"]*Li+
-    meanfish["groupingSHRAY"]+meanfish["Tzero"]*tz + meanfish["Temperature:LLinf"]*Te*Li + 
-    meanfish["Temperature:groupingSHRAY"]*Te
-  Q10data[4,2] <- (10^(ysshray[31])/10^(ysshray[1]))^(10/(30-0))
-  
-# now plot the temperature effects for the different guilds
-  
-  # first add metabolic Q10 of 2.5 from A = 3 at 0 degrees celsius
-  temp <- c(0,30)
-  metl <- c(log10(3),log10(3*2.5^3))
-  plot(metl~temp, lty=5,lwd=1,type="l", xlim=c(-2,30), ylim=c(0.4,1.7),col="black",
-       xaxt="n",yaxt="n", xlab=TeX("$Temperature$ ^{o}$C"),ylab="Growth coef. A")
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="PEL" & fishes$Linf >100])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="PEL" & fishes$Linf >100])
-  lines(ypel[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax],
-        col="#ef3b2c", lty=1,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="DEEP" & fishes$Linf >100])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="DEEP" & fishes$Linf >100])
-  lines(ydeep[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax],
-        col="#bdbdbd", lty=1,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="DEM" & fishes$Linf >100])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="DEM" & fishes$Linf >100])
-  lines(ydem[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax], 
-        col="#08519c", lty=1,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="SHRAY" & fishes$Linf >100])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="SHRAY" & fishes$Linf >100])
-  lines(yshray[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax],
-        col="black", lty=1,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="PEL" & fishes$Linf <30])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="PEL" & fishes$Linf <30])
-  lines(yspel[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax],
-        col="#ef3b2c", lty=5,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="DEEP" & fishes$Linf <30])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="DEEP" & fishes$Linf <30])
-  lines(ysdeep[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax],
-        col="#bdbdbd", lty=5,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="DEM" & fishes$Linf<30])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="DEM" & fishes$Linf <30])
-  lines(ysdem[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax], 
-        col="#08519c", lty=5,lwd=3)
-  
-  nbmin <- min(fishes$Temperature[fishes$grouping=="SHRAY" & fishes$Linf <30])
-  nbmax <- max(fishes$Temperature[fishes$grouping=="SHRAY" & fishes$Linf <30])
-  lines(ysshray[Te>nbmin & Te< nbmax]~Te[Te>nbmin & Te< nbmax],
-        col="black", lty=5,lwd=3)
-  
+# finish plotting
   axis(1,c(0,15,30))
-  axis(2,c(log10(2),log10(4),log10(8),log10(16),log10(32)),c("2","4","8","16","32"),las=1)
-  
-  legend(0,log10(45), legend=c("pelagics", "demersals", "elasmobranchs","deep-living"),
-         col=c("#ef3b2c","#08519c","black","#bdbdbd"), lty=c(1,1,1,1),lwd=c(2,2,2,2), cex=0.8,
-         box.lty=0)
-dev.off()
-  
+  axis(2,c(0,1,2),c("1","10","100"),las=1)
+  #text(5, 2.3, TeX("$Q_{10}$ = 1.4"), cex=1)
+ 
+# part B - within species response with random slope and intercept
 
+# get 95% CI for coefficients for M3 model
+  M3_prof <- profile(M3)
+  M3_unc <- confint(M3_prof,level=0.95)
+  
+# get random effect 
+  reff <- ranef(M3_LMER)
+  reff <- reff$Name
+  reff$Name <- rownames(reff)
+  rownames(reff) <- c()
+    
+# simulate 95 CI of random effects from merMod object posterior distributions 
+  resamp <- REsim(M3_LMER, n.sims = 1500)
+  resamp$Twithin_lower <- resamp$mean-1.96*resamp$sd
+  resamp$Twithin_upper <- resamp$mean+1.96*resamp$sd
+  resamp <- subset(resamp,resamp$term =="T_within" & resamp$groupFctr =="Name")
+  
+# bind data together
+  dat_within <- cbind(reff,resamp[match(reff$Name,resamp$groupID),c(7:8)])
+  dat_within$FE_Twithin <- fixef(M3)[2]
+  dat_within$FE_Twithin_lower <- M3_unc[7,1]
+  dat_within$FE_Twithin_upper <- M3_unc[7,2]
+    
+# estimate Q10 per species + 95% CI 
+  q10within <- as.data.frame(matrix(data=NA, ncol=5,nrow=nrow(dat_within)))
+  colnames(q10within) <- c("Name","q10","q10_lower","q10_upper","subset")
+  for (j in 1:771){
+    dd <- subset(fishes,fishes$Name ==  dat_within$Name[j])
+    q10within[j,5] <- ((nrow(dd)>1) & (max(dd$T_within)-min(dd$T_within) > 5))
+    within_RE <- dat_within[j,"T_within"]
+    within_FE <- dat_within[j,"FE_Twithin"]
+    within_low_RE <- dat_within[j,"Twithin_lower"]
+    within_low_FE <- dat_within[j,"FE_Twithin_lower"]
+    within_high_RE <- dat_within[j,"Twithin_upper"]
+    within_high_FE <- dat_within[j,"FE_Twithin_upper"]
+    dif <- c(-5,5)
+    y2 <- within_RE * dif + within_FE*dif
+    y2_low <- within_low_RE * dif + within_low_FE * dif
+    y2_high <- within_high_RE * dif + within_high_FE * dif
+    xd <- dif[2]-dif[1]
+    q10within[j,2] <- 10^((y2[2]-y2[1])/xd*10)
+    q10within[j,3] <- 10^((y2_low[2]-y2_low[1])/xd*10)
+    q10within[j,4] <- 10^((y2_high[2]-y2_high[1])/xd*10)
+    q10within[j,1] <- dat_within$Name[j]
+    }
+  
+  colMeans(q10within[2:4])
+  
+  q10within <- q10within[order(q10within$q10),]
+  subq10with <- subset(q10within,q10within$subset =="TRUE")
+  
+  plot(subq10with$q10,c(1:nrow(subq10with)),xlim=c(0,3.5),ylab="Species", 
+       xlab=TeX("$Q_{10}$"),las=1,yaxt="n",xaxt="n")
+  for (i in 1:nrow(subq10with)){
+    lines(c(subq10with$q10_lower[i],subq10with$q10_upper[i]),c(i,i),lwd=0.25)
+  }
+  points(subq10with$q10,c(1:nrow(subq10with)),pch=16,col="grey")
+  
+  axis(1,c(0,1,2,3))
+  axis(2,c(1,40,80),las=1)
+  lines(c(1.38,1.38),c(1,87),col="blue")
+  lines(c(1.28,1.28),c(1,87),col="red",lty=2)
+  lines(c(1.48,1.48),c(1,87),col="red",lty=2)
 
+  dev.off()
+  
